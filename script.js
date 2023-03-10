c82b2043430af74962775e12ecb4c40dd9c9302e
00013540 <ulugbek.tursunaliev1993@gmail.com>
2023-03-11 03:51:10 +0500

    All images made lazily-loaded

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js";
 
+new LazyLoad({});
+
 new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 16,
